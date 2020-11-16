df02278e27c779fef3e562ae2ed32f26df8702e9
Le Duy Anh <lenduya@gmail.com>
2020-11-16 14:44:26 +0100

    Initial commit

---
+++ app/src/main/java/com/example/primitivereader/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.primitivereader
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initLibraryFragment()
+    }
+
+    private fun initLibraryFragment() {
+        TODO()
+    }
+}
