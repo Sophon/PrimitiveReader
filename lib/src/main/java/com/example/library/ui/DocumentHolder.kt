2c1c5058ecca26864d74eec6cf1436b85cd6b3a2
Le Duy Anh <lenduya@gmail.com>
2020-11-17 17:05:21 +0100

    Holder: skeleton

---
+++ lib/src/main/java/com/example/library/ui/DocumentHolder.kt
@@ -0,0 +1,15 @@
+package com.example.library.ui
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.library.databinding.ItemDocumentBinding
+
+class DocumentHolder(private val binding: ItemDocumentBinding): RecyclerView.ViewHolder(binding.root) {
+
+    init {
+        TODO("bind viewmodel")
+    }
+
+    fun bind() {
+        TODO("implement")
+    }
+}
