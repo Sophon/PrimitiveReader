a2ea2f2376fb8dc2806697b4782e9bb1b01e89c2
Le Duy Anh <lenduya@gmail.com>
2020-11-17 17:07:49 +0100

    Adapter: skeleton

---
+++ lib/src/main/java/com/example/library/ui/LibraryAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.library.ui
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+
+class LibraryAdapter: ListAdapter<Document, DocumentHolder>(DiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DocumentHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: DocumentHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
+
+class DiffCallback: DiffUtil.ItemCallback<Document>() {
+    override fun areItemsTheSame(oldItem: Document, newItem: Document): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun areContentsTheSame(oldItem: Document, newItem: Document): Boolean {
+        TODO("Not yet implemented")
+    }
+}
