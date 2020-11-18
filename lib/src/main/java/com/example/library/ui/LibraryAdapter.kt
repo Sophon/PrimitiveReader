599599008e3f0ae224dab95a8521d33f30d169b4
Le Duy Anh <lenduya@gmail.com>
2020-11-18 01:26:37 +0100

    Implement adapter

--- lib/src/main/java/com/example/library/ui/LibraryAdapter.kt
+++ lib/src/main/java/com/example/library/ui/LibraryAdapter.kt
@@ -1,27 +1,35 @@
 package com.example.library.ui
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.example.core.data.Document
+import com.example.library.R
+import com.example.library.databinding.ItemDocumentBinding
 
-class LibraryAdapter: ListAdapter<Document, DocumentHolder>(DiffCallback()) {
+class LibraryAdapter(
+        val items: List<Document>
+): RecyclerView.Adapter<DocumentHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DocumentHolder {
-        TODO("Not yet implemented")
+        val binding = DataBindingUtil.inflate<ItemDocumentBinding>(
+                LayoutInflater.from(parent.context),
+                R.layout.item_document,
+                parent,
+                false
+        )
+
+        return DocumentHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
     }
 
     override fun onBindViewHolder(holder: DocumentHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
-}
-
-class DiffCallback: DiffUtil.ItemCallback<Document>() {
-    override fun areItemsTheSame(oldItem: Document, newItem: Document): Boolean {
-        TODO("Not yet implemented")
-    }
-
-    override fun areContentsTheSame(oldItem: Document, newItem: Document): Boolean {
-        TODO("Not yet implemented")
+        holder.bind(items[position])
     }
 }
