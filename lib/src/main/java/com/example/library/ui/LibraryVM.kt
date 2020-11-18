b263b567b7a3581a6f5cd76d8e761e1a963dfaf4
Le Duy Anh <lenduya@gmail.com>
2020-11-18 01:34:58 +0100

    Create and connect LibraryVM

---
+++ lib/src/main/java/com/example/library/ui/LibraryVM.kt
@@ -0,0 +1,11 @@
+package com.example.library.ui
+
+import androidx.lifecycle.ViewModel
+import com.example.core.data.Document
+
+class LibraryVM: ViewModel() {
+
+    fun onDocumentClicked(document: Document) {
+        TODO("implement")
+    }
+}
